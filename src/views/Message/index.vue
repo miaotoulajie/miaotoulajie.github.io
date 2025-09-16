217ba1c61dd65960f995a55d66be52f04543d712
Miao <910062657@qq.com>
2025-09-17 00:55:22 +0800

    feat: 添加 Message 组件并配置 Vite 基础路径
    - 新增 Message 组件，用于展示联系信息页面
    - 在 vite.config.js 中添加 base 配置项，以支持不同环境下的路径设置
    - 更新 .vite/deps/_metadata.json 文件，可能是由于依赖更新或项目配置变化

---
+++ src/views/Message/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="contact">
+    <h1>联系我</h1>
+    <p>可以通过邮箱、社交媒体等方式联系我。</p>
+    <router-link to="/" class="nav-link">返回首页</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Message'
+}
+</script>
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { resolve } from 'path' // 需要导入 path 模块
+import { resolve } from "path"; // 需要导入 path 模块
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src') // 设置 @ 指向 src 目录
-    }
-  }
+      "@": resolve(__dirname, "src"), // 设置 @ 指向 src 目录
+    },
+    base: process.env.NODE_ENV === "production" ? "/.github.io/" : "/",
+  },
 });
