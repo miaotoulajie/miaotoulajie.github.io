d76b6ca157364b5c63303ed0325707b742b30c55
Miao <910062657@qq.com>
2025-09-17 00:05:04 +0800

    feat: 初始化基于 Vite 的个人主页项目
    - 添加 Vite 项目结构和依赖
    - 创建空的 index.html 文件作为入口
    - 引入 esbuild、nanoid、rollup 等开发工具

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>你好，世界！</h1>
+    <p>这是我的 GitHub Pages 网站。</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+
+</style>
