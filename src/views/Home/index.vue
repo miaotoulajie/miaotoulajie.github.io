2746b322bbb128cebe4d1aeee70e7b8b44a09e2a
Miao <910062657@qq.com>
2025-09-18 00:26:27 +0800

    test: test deployment workflow

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,6 +3,7 @@
     <h1>欢迎来到我的主页</h1>
     <p>这是网站的主页面内容。</p>
     <router-link to="/about" class="nav-link">关于我</router-link>
+    <router-link to="/message" class="nav-messeage">留言板</router-link>
   </div>
 </template>
 
@@ -25,5 +26,15 @@ export default {
   color: white;
   text-decoration: none;
   border-radius: 5px;
+}
+.nav-messeage {
+  display: inline-block;
+  margin-top: 20px;
+  padding: 10px 20px;
+  background-color: #fff;
+  color: #42b883;
+  text-decoration: none;
+  border-radius: 5px;
+  margin-left: 8px;
 }
 </style>
